6c9826b43499a07253cdd6fe73226d3d4acafdfe
VadimAlexandru <v700.lg@gmail.com>
2021-01-06 13:11:33 +0200

    init

--- routes/botman.php
+++ routes/botman.php
@@ -13,6 +13,34 @@ $bot = resolve('botman');
 //
 //
 
+use BotMan\Drivers\Telegram\Extensions\Keyboard;
+use BotMan\Drivers\Telegram\Extensions\KeyboardButton;
+
+
+$keyboard = Keyboard::create()->type( Keyboard::TYPE_KEYBOARD )
+    ->oneTimeKeyboard(true)
+    ->addRow(
+        KeyboardButton::create("Да")->callbackData('first_inline'),
+        KeyboardButton::create("Нет")->callbackData('second_inline')
+    )
+    ->toArray();
+
+
+
+
+
+
+$bot->hears('/start', function(BotMan $bot) use ($keyboard) {
+    $bot->reply('starting', $keyboard);
+});
+
+
+
+
+
+
+
+
 $bot->hears('payload', function(BotMan $bot) {
     $bot->reply(
         $bot->getMessage()->getSender()
